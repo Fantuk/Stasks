142775288f8721f18c419920c58a24569bbb5b9c
Fanuk <kirillsvirid2006@gmail.com>
2025-11-24 18:09:23 +0700

    add auth/register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,13 +8,24 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("register")
-  register(@Body() body) {
-    console.log(body);
+  register(@Body() body: RegisterDto) {
+    return this.authService.register(body);
+  }
+
+  @Post("login")
+  login(@Body() body) {
+    return body;
+  }
+
+  @Post("logout")
+  logout() {
+    return "logout";
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { hashSync, genSaltSync } from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(registerData: RegisterDto) {
+    const { password: plainPassword, email } = registerData;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException({
+        message: 'Пользователь с таким email уже существует',
+      });
+    }
+
+    const salt = genSaltSync(Number(process.env.SALT));
+    const hashedPassword = hashSync(plainPassword, salt);
+
+    const createdUser = await this.prisma.user
+      .create({
+        data: {
+          ...registerData,
+          password: hashedPassword,
+        },
+      })
+      .catch((error) => {
+        throw new BadRequestException(error.message);
+      });
+
+    const { password, ...result } = createdUser;
+    
+    return result;
+  }
+}
