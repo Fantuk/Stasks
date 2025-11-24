f7ec8dd1d8668fc646ea076198b565e5278ce549
Fanuk <kirillsvirid2006@gmail.com>
2025-11-24 19:18:50 +0700

    move logic into user service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,45 +1,21 @@
 import {
-  BadRequestException,
   ConflictException,
   Injectable,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { RegisterDto } from './dto/register.dto';
-import { hashSync, genSaltSync } from 'bcrypt';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly userService: UserService,
+  ) {}
 
-  async register(registerData: RegisterDto) {
-    const { password: plainPassword, email } = registerData;
-
-    const existingUser = await this.prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (existingUser) {
-      throw new ConflictException({
-        message: 'Пользователь с таким email уже существует',
-      });
+  async register(registerData: CreateUserDto) {
+    const user = await this.userService.findByEmail(registerData.email);
+    if (user) {
+      throw new ConflictException('Пользователь с таким email уже существует');
     }
-
-    const salt = genSaltSync(Number(process.env.SALT));
-    const hashedPassword = hashSync(plainPassword, salt);
-
-    const createdUser = await this.prisma.user
-      .create({
-        data: {
-          ...registerData,
-          password: hashedPassword,
-        },
-      })
-      .catch((error) => {
-        throw new BadRequestException(error.message);
-      });
-
-    const { password, ...result } = createdUser;
-    
-    return result;
+    return this.userService.create(registerData);
   }
 }
