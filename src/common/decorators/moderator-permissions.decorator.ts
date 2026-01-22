f6fc06acd08e5ca3e0612c7b2f1739b8076c2a55
Fanuk <kirillsvirid2006@gmail.com>
2026-01-22 16:45:54 +0700

    add moderator permissions decorator

---
+++ src/common/decorators/moderator-permissions.decorator.ts
@@ -0,0 +1,8 @@
+import { SetMetadata } from '@nestjs/common';
+import { IModeratorAccessRights } from 'src/moderator/domain/entities/moderator.entity';
+
+export const MODERATOR_PERMISSIONS_KEY = 'moderatorPermissions'
+
+export const ModeratorPermissions = (
+    ...permissions: Array<keyof IModeratorAccessRights>
+) => SetMetadata(MODERATOR_PERMISSIONS_KEY, permissions);
