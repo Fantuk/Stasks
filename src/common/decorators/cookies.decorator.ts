c8f4d99e5898acc75495005252a9cd37778a6119
Fanuk <kirillsvirid2006@gmail.com>
2026-01-22 16:45:52 +0700

    add cookies decorator

---
+++ src/common/decorators/cookies.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Cookies = createParamDecorator(
+  (key: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const { cookies } = request;
+    return key ? cookies?.[key] : cookies;
+  },
+);
