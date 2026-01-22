8f86a46fe9a7b1e5b9584a3ee3b870a404f79cbc
Fanuk <kirillsvirid2006@gmail.com>
2026-01-22 16:49:11 +0700

    add user application interfaces

---
+++ src/user/application/interfaces/interfaces.ts
@@ -0,0 +1,39 @@
+import { Role } from '@prisma/client';
+import { ICreateModeratorParams } from 'src/moderator/application/interfaces/interfaces';
+import { IModeratorAccessRights } from 'src/moderator/domain/entities/moderator.entity';
+import { ICreateStudentParams } from 'src/student/application/interfaces/interfaces';
+import { ICreateTeacherParams } from 'src/teacher/application/interfaces/interfaces';
+
+export interface ICreateUserParams {
+  institutionId?: number;
+  name: string;
+  surname: string;
+  patronymic: string | null;
+  email: string;
+  password: string;
+  roles: Role[];
+  isActivated?: boolean;
+
+  // Специфичные данные для ролей
+  moderatorData?: { accessRights?: IModeratorAccessRights };
+  teacherData?: { mentoredGroupId?: number };
+  studentData?: { groupId?: number };
+}
+
+export type UserResponse = {
+  id: number | null;
+  institutionId: number;
+  name: string;
+  surname: string;
+  patronymic: string | null;
+  email: string;
+  roles: Role[];
+  isActivated: boolean;
+};
+
+export interface IFullUser {
+  user: UserResponse;
+  moderator?: ICreateModeratorParams | null;
+  teacher?: ICreateTeacherParams | null;
+  student?: ICreateStudentParams | null;
+}
