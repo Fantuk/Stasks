b9002fe2bf40f2f6494387ff7f6d399e5af2e467
Fanuk <kirillsvirid2006@gmail.com>
2026-01-22 16:44:24 +0700

    update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from 'src/user/application/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { jwtModuleAsyncOptions } from 'src/configs/jwt-module.config';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { TokenModule } from 'src/token/application/token.module';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UserModule],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync(jwtModuleAsyncOptions()),
+    PassportModule,
+    TokenModule,
+  ],
 })
 export class AuthModule {}
