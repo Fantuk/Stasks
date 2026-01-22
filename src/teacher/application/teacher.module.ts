aab9c298136643e23c7ee626811102d72205e344
Fanuk <kirillsvirid2006@gmail.com>
2026-01-22 16:48:58 +0700

    add teacher module

---
+++ src/teacher/application/teacher.module.ts
@@ -0,0 +1,21 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { TeacherService } from './teacher.service';
+import { TeacherController } from 'src/teacher/application/teacher.controller';
+import { TeacherRepository } from 'src/teacher/infrastructure/prisma/teacher.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserModule } from 'src/user/application/user.module';
+
+@Module({
+  controllers: [TeacherController],
+  providers: [
+    TeacherService,
+    PrismaService,
+    {
+      provide: 'TeacherRepository',
+      useClass: TeacherRepository,
+    },
+  ],
+  exports: [TeacherService],
+  imports: [forwardRef(() => UserModule)],
+})
+export class TeacherModule {}
