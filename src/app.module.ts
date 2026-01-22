287484d069a08086d92b9ecc7ab3ed24be0eac88
Fanuk <kirillsvirid2006@gmail.com>
2026-01-22 16:44:20 +0700

    add new modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/application/user.module';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { TokenModule } from 'src/token/application/token.module';
+import { ModeratorModule } from 'src/moderator/application/moderator.module';
+import { TeacherModule } from 'src/teacher/application/teacher.module';
+import { StudentModule } from 'src/student/application/student.module';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+import { ResponseTransformInterceptor } from 'src/common/interceptors/response-transform.interceptor';
+import { ModeratorPermissionsGuard } from 'src/common/guards/moderator-permissions.guard';
 
 @Module({
   imports: [
@@ -12,8 +22,29 @@ import { UserModule } from './user/user.module';
     PrismaModule,
     AuthModule,
     UserModule,
+    TokenModule,
+    ModeratorModule,
+    TeacherModule,
+    StudentModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseTransformInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
