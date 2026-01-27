3aeafc5b04b139d5b48c48893b62d4e405018d71
Fanuk <kirillsvirid2006@gmail.com>
2026-01-27 16:33:10 +0700

    update group module: wire repository, student and teacher modules

--- src/group/application/group.module.ts
+++ src/group/application/group.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { GroupService } from 'src/group/application/group.service';
 import { GroupController } from 'src/group/application/group.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { GroupRepository } from 'src/group/infrastructure/prisma/group.repository';
+import { TeacherModule } from 'src/teacher/application/teacher.module';
+import { StudentModule } from 'src/student/application/student.module';
+
 
 @Module({
   controllers: [GroupController],
-  providers: [GroupService],
+  providers: [
+    GroupService,
+    PrismaService,
+    {
+      provide: 'GroupRepository',
+      useClass: GroupRepository,
+    },
+  ],
+  exports: [GroupService, 'GroupRepository'],
+  imports: [
+    forwardRef(() => TeacherModule), 
+    forwardRef(() => StudentModule)
+  ]
 })
-export class GroupModule {}
+export class GroupModule { }
