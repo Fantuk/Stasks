6a90a80d1e0d19b4b1fb092ef305fe7476708764
Fanuk <kirillsvirid2006@gmail.com>
2026-03-01 17:57:14 +0700

    update get-me dto

--- src/user/application/dto/get-me.dto.ts
+++ src/user/application/dto/get-me.dto.ts
@@ -1,6 +1,9 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
+/** Query DTO для GET /users/me */
 export class GetMeDto {
+  @ApiPropertyOptional({ description: 'Включить связанные сущности', example: 'roles' })
   @IsOptional()
   @IsString()
   include?: string;
