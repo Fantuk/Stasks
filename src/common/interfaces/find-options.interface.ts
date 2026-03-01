f041d96318bce50f4cdff52eb7411d66afadd192
Fanuk <kirillsvirid2006@gmail.com>
2026-03-01 17:55:47 +0700

    update find-options interface

--- src/common/interfaces/find-options.interface.ts
+++ src/common/interfaces/find-options.interface.ts
@@ -10,3 +10,21 @@ export type UserProfileIncludeOption = 'student' | 'teacher' | 'moderator';
 export interface IFindUserOptions {
   include?: UserProfileIncludeOption[];
 }
+
+export type BuildingIncludeOption = 'floors' | 'floors.classrooms';
+
+export interface IBuildingFindOptions {
+  include?: BuildingIncludeOption[];
+}
+
+export type FloorIncludeOption = 'classrooms';
+
+export interface IFloorFindOptions {
+  include?: FloorIncludeOption[];
+}
+
+export type ClassroomIncludeOption = 'floor';
+
+export interface IClassroomFindOptions {
+  include?: ClassroomIncludeOption[];
+}
