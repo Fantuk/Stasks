4d76c4cb8f10439aee329b10195d1a2b89f2ab8f
Fanuk <kirillsvirid2006@gmail.com>
2026-03-01 17:58:24 +0700

    update create-group dto

--- src/group/application/dto/create-group.dto.ts
+++ src/group/application/dto/create-group.dto.ts
@@ -1,6 +1,9 @@
-import { IsNotEmpty, IsString, Length } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
+/** DTO создания группы */
 export class CreateGroupDto {
+    @ApiProperty({ example: 'ИС-41', minLength: 2, maxLength: 50, description: 'Название группы' })
     @IsNotEmpty({ message: 'Название группы обязательно' })
     @IsString({ message: 'Название должно быть строкой' })
     @Length(2, 50, { message: 'Название должно быть от 2 до 50 символов' })
