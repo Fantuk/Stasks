0e3f0c53e4344edb43697dabcbd92a144137aee8
Fanuk <kirillsvirid2006@gmail.com>
2026-03-01 18:00:08 +0700

    add classroom entity

---
+++ src/classroom/domain/entities/classroom.entity.ts
@@ -0,0 +1,35 @@
+export class Classroom {
+  private constructor(
+    public readonly id: number | null,
+    public readonly floorId: number,
+    public name: string,
+  ) {}
+
+  static create(params: { floorId: number; name: string }): Classroom {
+    return new Classroom(null, params.floorId, params.name);
+  }
+
+  static fromPersistence(raw: {
+    id: number;
+    floorId: number;
+    name: string;
+  }): Classroom {
+    return new Classroom(raw.id, raw.floorId, raw.name);
+  }
+
+  toPersistence() {
+    return {
+      id: this.id ?? undefined,
+      floorId: this.floorId,
+      name: this.name,
+    };
+  }
+
+  toResponse() {
+    return {
+      id: this.id,
+      floorId: this.floorId,
+      name: this.name,
+    };
+  }
+}
