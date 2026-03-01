c846d7bbb9af1909c94799ea363cd662eb995624
Fanuk <kirillsvirid2006@gmail.com>
2026-03-01 17:58:02 +0700

    update assign-group dto

--- src/teacher/application/dto/assign-group.dto.ts
+++ src/teacher/application/dto/assign-group.dto.ts
@@ -1,7 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsPositive } from 'class-validator';
 
+/** DTO назначения группы (куратору или студенту) */
 export class AssignGroupDto {
+  @ApiProperty({ example: 1, description: 'ID группы' })
   @IsNumber()
-  @IsPositive({ message: 'Id группы должно быть больше положительным' })
+  @IsPositive({ message: 'Id группы должно быть положительным' })
   groupId: number;
 }
