2d2979201e859e40dad7a7b01aebf620dd79b90b
Fanuk <kirillsvirid2006@gmail.com>
2026-03-01 17:55:39 +0700

    update search-query dto

--- src/common/dto/search-query.dto.ts
+++ src/common/dto/search-query.dto.ts
@@ -1,7 +1,9 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { PaginationDto } from './pagination.dto';
 import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class SearchQueryDto extends PaginationDto {
+  @ApiPropertyOptional({ description: 'Поисковая строка (фильтр по имени/названию)', example: 'Иван' })
   @IsOptional()
   @IsString()
   @MinLength(1, { message: 'Поисковый запрос не может быть пустым' })
